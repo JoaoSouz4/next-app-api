fb831cde9851b5e38ab399df84d0d27386b364d0
JoaoSouz4 <joaosouz8@gmail.com>
2025-03-24 00:30:20 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors(
     {
-      origin: ['https://next-app-nu-neon.vercel.app/', 'http://localhost:3000'], // Permitir apenas este domínio
+      origin: ['https://next-app-nu-neon.vercel.app', 'http://localhost:3000'], // Permitir apenas este domínio
       methods: 'GET,POST,PUT,DELETE',
       allowedHeaders: 'Content-Type,Authorization'
     },
